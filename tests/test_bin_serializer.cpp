e772120817035097cacc1084437242740cc1e26d
ousttrue <ousttrue@gmail.com>
2023-09-15 22:34:56 +0900

    fix tests

--- tests/test_bin_serializer.cpp
+++ tests/test_bin_serializer.cpp
@@ -6,6 +6,7 @@
 TEST(TestBinSerializer, BinSerializer)
 {
   auto json = gltfjson::tree::NewNode(gltfjson::tree::ObjectValue{});
+  auto buffers = json->SetProperty(u8"buffers", gltfjson::tree::ArrayValue{});
   auto bufferViews =
     json->SetProperty(u8"bufferViews", gltfjson::tree::ArrayValue{});
   auto accessors =
@@ -17,17 +18,21 @@ TEST(TestBinSerializer, BinSerializer)
 
   uint8_t data[] = { 1, 2, 3, 4 };
   auto [offset, length] = w.PushBufferView(data);
-  auto buffer = bufferViews->Add(gltfjson::tree::ObjectValue{});
-  buffer->SetProperty(u8"buffer", (float)0);
-  buffer->SetProperty(u8"offset", (float)offset);
-  buffer->SetProperty(u8"length", (float)length);
+  auto buffer = buffers->Add(gltfjson::tree::ObjectValue{});
+  buffer->SetProperty(u8"byteLength", (float)4);
+
+  auto bufferView = bufferViews->Add(gltfjson::tree::ObjectValue{});
+  bufferView->SetProperty(u8"buffer", (float)0);
+  bufferView->SetProperty(u8"byteOffset", (float)offset);
+  bufferView->SetProperty(u8"byteLength", (float)length);
 
   auto accessor = accessors->Add(gltfjson::tree::ObjectValue{});
   accessor->SetProperty(u8"bufferView", (float)0);
   accessor->SetProperty(u8"type", u8"SCALAR");
   accessor->SetProperty(u8"componentType", (float)5121);
+  accessor->SetProperty(u8"count", (float)4);
 
-  gltfjson::Bin bin;
+  gltfjson::Bin bin{ .Bytes = src };
   std::vector<uint8_t> dst;
   gltfjson::tree::BinSerializer s(gltf, bin, dst);
   s.Serialize({}, {});
